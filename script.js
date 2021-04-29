768da3cbc9da65c52b28c84f50cd8948b892d02f
Daniel Kim <kkdh1109@gmail.com>
2021-04-29 15:27:59 -0400

    loop thr animationArr to capture the position of each img

--- script.js
+++ script.js
@@ -7,17 +7,86 @@ const playerImg = new Image();
 playerImg.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
+// playerImg.src = 'stick.png';
+// const spriteWidth = 23;
+// const spriteHeight = 340;
 
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    }
+]
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+
+    for (let i = 0; i < state.frames; i++) {
+        let positionX = i * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+
+    spriteAnimations[state.name] = frames;
+});
 
 function animate() {
     ctx.clearRect(0, 0, CAN_WIDTH, CAN_HEIGHT);
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations['idle'].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations['idle'].loc[position].y;
 
-    // ctx.fillRect(50, 50, 100, 100);
-    // use the first 4 values to cut a portion of img from a sprite sheet 
-    //last 4 values are for starting position x and y and size of the img on the canvas
-    ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+     ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+   
+     //slowing down the animation
+    // if (gameFrame % staggerFrames == 0) {
+    //     if (frameX < 5) frameX++;
+    //     else frameX = 0;
+    // }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 
 };
